aa314e04291373d74a358b52ec7d9227046bb9ac
Sudeep Juvekar <sjuvekar@gmail.com>
2012-07-15 00:11:15 -0400

    Improved script

--- www/scripts/iglassdoor-main.js
+++ www/scripts/iglassdoor-main.js
@@ -2,6 +2,9 @@
 var __LOCATION_SEARCH_URL__ = "http://www.glassdoor.com/findLocationsByFullTextAjax.htm?locationSearchString="
 var __HARDCODED_COOKIE__ = '"c3VkanV2ZWthckB5YWhvby5jb206MTM3Mjg5NDg1MTIzMjpmMWY3NjVmZTRhZjkzNjM1YzQ4ZGQ5ODQ5MmU4YTBjMA=="'
 
+// Glassdoor main url
+var __GLASSDOOR_URL__ = "http://www.glassdoor.com"
+
 /// A hash table for maintaining search get urls
 var __SEARCH_GET_URLS__ = "http://www.iglassdoor-sjuvekar.appspot.com/search?"
 
@@ -18,23 +21,65 @@ var setCookie = function(cookieName) {
 }
 
 /// A method to handle click event for a given tab. Pass appropriate tab string name
-var handleTabClick = function(tabName) {
-	company = $("#search-" + tabName).val()	
-    company = company.replace(/\s/g, "-")
-	$("#results-" + tabName).empty()
-    url = __SEARCH_GET_URLS__ + "type=" + tabName + "&company=" + company + "&callback=?"
-	$.getJSON(url, function(data){ 
-		$.each(data, function(i, item) {
-			$("#results-" + tabName).
-				append($("<li></li>")
-					.append($("<a></a>").attr("href", url)
-						.append($("<h1></h1>").text(item)
-			)));
-		});
-	});
-	$("#results-" + tabName).listview("refresh");
+var handleJobsClick = function(tabName) {
+    url = getURL(tabName);
+    $("#results-" + tabName).empty();
+    $.getJSON(url, function(data){ 
+        jobs = data.jobs_list;
+        for (var i = 0; i < jobs.length; i++) {
+            $("#results-" + tabName).
+              append($("<li></li>")
+                .append($("<a></a>").attr("href", __GLASSDOOR_URL__ + jobs[i].url)
+                    .append($("<h1></h1>").text(jobs[i].title)
+                        .append($("<p></p>").text(jobs[i].location).css("margin-top", "10px").css("margin-left", "10px").css("white-space", "normal")
+            ))));
+            $("#results-" + tabName).listview("refresh");
+        }
+    });
 	$("#search-" + tabName + "-collapsible").trigger("collapse");
+}
 
+var handleSalariesClick = function(tabName) {
+    url = getURL(tabName);
+    $("#results-" + tabName).empty();
+    $.getJSON(url, function(data){ 
+        salaries = data.salaries_list;
+        for (var i = 0; i < salaries.length; i++) {
+            $("#results-" + tabName).
+              append($("<li></li>")
+                .append($("<a></a>").attr("href", __GLASSDOOR_URL__ + salaries[i].url)
+                    .append($("<h1></h1>").text(salaries[i].title)
+                        .append($("<p></p>").text(salaries[i].num_reports).css("margin-top", "10px").css("margin-left", "10px").css("white-space", "normal")
+            ))));
+            $("#results-" + tabName).listview("refresh");
+        }
+    });
+	$("#search-" + tabName + "-collapsible").trigger("collapse");
+}
+
+var handleInterviewsClick = function(tabName) {
+    url = getURL(tabName);
+    $("#results-" + tabName).empty();
+    $.getJSON(url, function(data){ 
+        interviews = data.interviews_list;
+        for (var i = 0; i < interviews.length; i++) {
+            $("#results-" + tabName).
+              append($("<li></li>")
+                .append($("<a></a>").attr("href", __GLASSDOOR_URL__ + interviews[i].url)
+                    .append($("<h1></h1>").text(interviews[i].title)
+                        .append($("<p></p>").text(interviews[i].description).css("margin-top", "10px").css("margin-left", "10px").css("white-space", "normal")
+            ))));
+            $("#results-" + tabName).listview("refresh");
+        }
+    });
+	$("#search-" + tabName + "-collapsible").trigger("collapse");
+}
+
+var getURL = function(tabName) {
+	company = $("#search-" + tabName).val()	;
+    company = company.replace(/\s/g, "-");
+    url = __SEARCH_GET_URLS__ + "type=" + tabName + "&company=" + company + "&callback=";
+    return url;
 };
 
 /// A function to handle blog click
@@ -69,22 +114,22 @@ var handleTwitterClick = function() {
 				.append($("<li></li>")
 					.append($("<a></a>").attr("href", "http://www.twitter.com/" + username + "/status/" + item.id_str)						
 						.append($("<img />").attr("src", "icons/twitter-list.png"))
-						.append($("<strong></strong>").html("Glassdoor&nbsp;"))
-							.append($("<i></i>").css("font-weight", "normal").css("font-size", "10px").html("@Glassdoordotcom"))
-                            .append($("<p></p>").css("margin-top", "10px").css("margin-left", "10px").css("white-space", "normal").html(item.text)
+							.append($("<strong></strong>").html("Glassdoor&nbsp;"))
+								.append($("<i></i>").css("font-weight", "normal").css("font-size", "10px").html("@Glassdoordotcom"))
+									.append($("<p></p>").css("margin-top", "10px").css("margin-left", "10px").css("white-space", "normal").html(item.text)
 			)));
-                 });
+		});
 		$("#results-twitter").listview("refresh")         
-           });
+	});
 };
 
 /// A global document oninit method
 $(document).bind("pageinit", function(event) {
                  
-    $("#search-jobs-button").click(  function() { handleTabClick("jobs"); });
+    $("#search-jobs-button").click(  function() { handleJobsClick("jobs"); });
     $("#search-companies-button").click(  function() { handleTabClick("companies"); });
-    $("#search-salaries-button").click(  function() { handleTabClick("salaries"); });
-    $("#search-interviews-button").click(  function() { handleTabClick("interviews"); });
+    $("#search-salaries-button").click(  function() { handleSalariesClick("salaries"); });
+    $("#search-interviews-button").click(  function() { handleInterviewsClick("interviews"); });
 
     /// Events after expanding the collapsible search button
     $("#search-companies-collapsible").bind("expand", function() {
