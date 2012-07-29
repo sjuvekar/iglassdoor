b878945993698daa8813b191c8badf5b66f1a03f
Sudeep Juvekar <sjuvekar@gmail.com>
2012-07-28 22:21:23 -0400

    interviews minus all details

--- www/scripts/iglassdoor-interviews.js
+++ www/scripts/iglassdoor-interviews.js
@@ -1,7 +1,7 @@
 var createInterviewCompanyDomElement = function(interviewCompanyListing) {
 	var title = "";
 	var url = "";
-	description = "";
+	var description = "";
 	companyDataLink = $(interviewCompanyListing).find("h3.tightTop").find("a");
 	companyData = $(interviewCompanyListing).find("p.tightBot");
 	if (companyDataLink == undefined && companyData == undefined)
@@ -20,6 +20,39 @@ var createInterviewCompanyDomElement = function(interviewCompanyListing) {
 	return el;
 }
 
+
+var createInterviewElement = function(interviewQuestion) {
+	var title = "";
+	var url = "";
+	var description = "";
+	interview_title = $(interviewQuestion).find("h3").find("a");
+	interview_detail = $(interviewQuestion).find("p.questionText");
+	if (interview_title == undefined && interview_detail == undefined)
+		return undefined;
+	if (interview_title) {
+		title = interview_title.text();
+		url = interview_title.attr("href");
+	}
+	if (interview_detail)
+		description = interview_detail.text();
+	el = $("<li></li>")
+	.append($("<a></a>").attr("href", __GLASSDOOR_URL__ + url).attr("target", "_blank")
+		.append($("<h1></h1>").html(title).css("white-space", "normal"))
+			.append($("<p></p>").html(description).css("white-space", "normal"))
+	);
+	return el;
+}
+
+var printEmployerHeader = function(return_html) {
+	if (return_html.find("p.seeAllEmployersLink").length > 0) {
+		all_employers = return_html.find("p.seeAllEmployersLink").find("a");
+		el = $("<a></a>").attr("href", all_employers.attr("href")).attr("target", "_blank")
+				.append($("<h2>" + all_employers.text() + "</h2>"));
+		return el;
+	}
+	return undefined;
+}
+
 var handleInterviewsClick = function(tabName) {
 	$.mobile.showPageLoadingMsg();
     clearStuff(tabName);
@@ -27,12 +60,9 @@ var handleInterviewsClick = function(tabName) {
     $.getJSON(url, function(data){ 
     	$.mobile.hidePageLoadingMsg();
     	return_html = $(data.contents);
-    	all_employers = return_html.find("p.seeAllEmployersLink").find("a");
-    	if (all_employers)
-    		$("#results-" + tabName + "-header")
-    			.append($("<a></a>").attr("href", all_employers.attr("href")).attr("target", "_blank")
-    				.append($("<h3>" + all_employers.text() + "</h3>"))
-    		);
+    	el = printEmployerHeader(return_html)
+    	if (el)
+    		$("#results-" + tabName + "-header").append(el)
     	searchResultCf = return_html.find(".searchResult.cf");
         for (var i = 0; i < searchResultCf.length; i++) {
         	el = createInterviewCompanyDomElement(searchResultCf[i]);
@@ -40,7 +70,14 @@ var handleInterviewsClick = function(tabName) {
         	$("#results-" + tabName).append(el);
             $("#results-" + tabName).listview("refresh");
         }
-        
+        $("#results-" + tabName + "-header-alt").append($("<h2>More Interview Questions</h2>"))
+        interviewQuestionList = return_html.find("div#InterviewQuestionResult_1");
+        for(var i = 0; i < interviewQuestionList.length; i++) {
+        	el = createInterviewElement(interviewQuestionList[i]);
+        	if (el == undefined) continue;
+        	$("#results-" + tabName + "-alt").append(el);
+            $("#results-" + tabName + "-alt").listview("refresh");
+        }
         addPageLinks(data, tabName)
     });
 	$("#search-" + tabName + "-collapsible").trigger("collapse");
